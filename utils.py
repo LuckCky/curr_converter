30042e231fc2e0c429b0074ba0a2216719663d37
LuckCky <www.aivanov@gmail.com>
2017-05-12 01:27:34 +0300

    comments added

--- utils.py
+++ utils.py
@@ -5,16 +5,25 @@ from config import ecb_url, cube, symbols
 
 
 def parse_currency_args(currency):
+    """
+    checks if we got symbol, nor three letter currency name
+    :param currency: user input symbol or three letter currency name
+    :return: currency symbol or None
+    """
     if currency and len(currency) < 3:
         currency = next((element['cc'] for element in symbols if element['symbol'] == currency), currency)
     return currency
 
 
 def parse_rate():
+    """
+    gets currency rates from ECB
+    :return: dict with error or rates, boolean for error
+    """
     try:
         response = requests.get(ecb_url)
     except Exception as e:
-        return {"error": "error occurred while accessing www.ecb.europa.eu: {}".format(e)}
+        return {"error": "error occurred while accessing www.ecb.europa.eu: {}".format(e)}, True
     else:
         currency_xml = response.content.decode()
     root = ET.fromstring(currency_xml)
@@ -22,4 +31,4 @@ def parse_rate():
     rates_list = [float(currency.attrib.get('rate')) for currency in root.iter(cube) if currency.attrib.get('rate')]
     result = dict(zip(currencies_list, rates_list))
     result['EUR'] = float(1)
-    return result
+    return result, False
