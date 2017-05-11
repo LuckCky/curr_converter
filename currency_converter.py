de9af51704e73d7cf6ae3674a21aa4e506ba0a79
LuckCky <www.aivanov@gmail.com>
2017-05-12 01:27:15 +0300

    comments and check_valid added

--- currency_converter.py
+++ currency_converter.py
@@ -5,6 +5,10 @@ from utils import parse_currency_args, parse_rate
 
 
 def create_parser():
+    """
+    creates parser for arguments
+    :return: parser with provided arguments
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--amount", type=float, required=True,
                         help="amount which we want to convert")
@@ -15,30 +19,42 @@ def create_parser():
     return parser
 
 
-def converter(input_amount, input_currency, output_currency):
-
+def check_input(input_amount, input_currency, rate):
+    """
+    various checks for user input
+    :param input_amount: amount which we want to convert
+    :param input_currency: 3 letters name or currency symbol
+    :param rate: currency rates from ECB
+    :return: error or False if all checks are good
+    """
     if input_amount <= 0:
-        result = {"input": "error! Currency amount cannot be negative or zero"}
+        result = {"input": "error! Input currency amount cannot be negative or zero"}
         print(json.dumps(result))
         return json.dumps(result)
+    elif not input_currency:
+        result = {"input": "error! No input currency provided"}
+        print(json.dumps(result))
+        return json.dumps(result)
+    try:
+        rate[input_currency]
+    except KeyError:
+        result = {"input": "error! Unknown input currency provided"}
+        print(json.dumps(result))
+        return json.dumps(result)
+    return False
 
-    # change symbols for three letter name
-    input_currency = parse_currency_args(input_currency)
-    output_currency = parse_currency_args(output_currency)
-    rate = parse_rate()
-    try:
-        result = {"input": rate["error"]}
-        print(json.dumps(result))
-        return json.dumps(result)
-    except KeyError:
-        pass
+
+def converter(input_amount, input_currency, output_currency, rate):
+    """
+    converts currencies
+    :param input_amount: amount which we want to convert
+    :param input_currency: 3 letters name or currency symbol
+    :param output_currency: 3 letters name or currency symbol
+    :param rate: dict with currency rates from ECB
+    :return: JSON with converted currencies
+    """
+    eur_rate = rate[input_currency]
     result = {}
-    try:
-        eur_rate = rate[input_currency]
-    except KeyError:
-        result = {"input": "error! No currency provided"}
-        print(json.dumps(result))
-        return json.dumps(result)
     try:
         curr_rate = rate[output_currency]
         output_amount = round(input_amount / eur_rate * curr_rate, 2)
@@ -67,9 +83,29 @@ def converter(input_amount, input_currency, output_currency):
 
 
 def main():
+    """
+    main func does all the job
+    :return: JSON with error if checks are unsuccessful or converted currencies
+    """
     parser = create_parser()
     args = parser.parse_args()
-    converter(args.amount, args.input_currency, args.output_currency)
+
+    # change symbols for three letter name
+    input_currency = parse_currency_args(args.input_currency)
+    output_currency = parse_currency_args(args.output_currency)
+
+    input_amount = args.amount
+
+    # get ECB rates
+    rate, rate_error = parse_rate()
+    if rate_error:
+        return rate
+    # check input conditions
+    input_error = check_input(input_amount, input_currency, rate)
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', input_error)
+    if input_error:
+        return input_error
+    return converter(input_amount, input_currency, output_currency, rate)
 
 if __name__ == '__main__':
     main()
